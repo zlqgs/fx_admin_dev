aa2911924dd230297c71093ad0949bf5e0f354fd
soyo <tuskshenyang@163.com>
2017-07-20 11:44:00 +0800

    修改代码样式，完善页面内容

--- src/app/head/head.vue
+++ src/app/head/head.vue
@@ -1,30 +1,66 @@
 <template>
     <div class="main-banner">
         <h4>页头样式</h4>
-        <div class="filter-container">
-            <div class="logo-container">
-                <a href="javascript:void(0);">
-                    <img class="logo" :src="logo_url"/>
-                    <div class="logo-words">
-                        <div class="logo-title">风箱网</div>
-                        <div class="logo-word">后台管理系统</div>
-                    </div>
-                </a>
-            </div>
-            <div class="menu-link"><a class="head-a" href="javascript:void(0);">测试链接</a><span class="split"></span><span>测试标题</span></div>
-            <div class="login-info">
-                <div class="user-name">
-                    测试用户
+        <div style="overflow: hidden">
+            <div class="filter-container">
+                <div class="logo-container">
+                    <a href="javascript:void(0);">
+                        <img class="logo" :src="logo_url"/>
+                        <div class="logo-words">
+                            <div class="logo-title">风箱网</div>
+                            <div class="logo-word">后台管理系统</div>
+                        </div>
+                    </a>
                 </div>
-                <span class="split"></span>
-                <a class="logout" href="javascript:void(0);">退出</a>
+                <div class="menu-link"><a class="head-a" href="javascript:void(0);">测试链接</a><span class="split"></span><span>测试标题</span></div>
+                <div class="login-info">
+                    <div class="user-name">
+                        测试用户
+                    </div>
+                    <span class="split"></span>
+                    <a class="logout" href="javascript:void(0);">退出</a>
+                </div>
             </div>
         </div>
         <h4>页头结构</h4>
-        <div class="structure-box">
-            <img :src="structure_url" alt="">
+        <div class="banner-box">
+            <div class="structure-box">
+                <img :src="structure_url" alt="">
+            </div>
         </div>
-        <h4>容器</h4>
+
+        <h4>页头代码</h4>
+        <div v-highlight>
+            <pre>
+                <code class="xml">
+                     <div class="filter-container">
+                         //logo模块
+                         <div class="logo-container">
+                             <a href="javascript:void(0);">
+                                 <img class="logo" :src="logo_url"/>
+                                 <div class="logo-words">
+                                     <div class="logo-title">风箱网</div>
+                                     <div class="logo-word">后台管理系统</div>
+                                 </div>
+                             </a>
+                         </div>
+                         //--
+                         //链接菜单
+                         <div class="menu-link"><a class="head-a" href="javascript:void(0);">测试链接</a><span class="split"></span><span>测试标题</span></div>
+                         //--
+                         //登录信息
+                         <div class="login-info">
+                             <div class="user-name">
+                                 测试用户
+                             </div>
+                             <span class="split"></span>
+                             <a class="logout" href="javascript:void(0);">退出</a>
+                         </div>
+                         //--
+                    </div>
+                </code>
+            </pre>
+        </div>
     </div>
 
 </template>
@@ -41,8 +77,3 @@
         }
     }
 </script>
-<style>
-    .main-banner{
-        width: 100%;
-    }
-</style>
