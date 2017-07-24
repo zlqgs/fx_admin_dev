61b786c6798a4143546b5d840953037935fae85d
soyo <tuskshenyang@163.com>
2017-07-24 14:35:45 +0800

    路由添加 highlights引用

--- src/main.js
+++ src/main.js
@@ -3,12 +3,37 @@ import VueRouter from 'vue-router'
 import Menu from './app/menu/Menu.vue'
 import head from './app/head/head.vue'
 import Footer from './app/footer/footer.vue';
+import nav from './app/nav/nav.vue';
+import cell from './app/cell/cell.vue';
+import hljs from 'highlight.js'
+import 'highlight.js/styles/railscasts.css' //样式文件
 
 Vue.use(VueRouter);
+Vue.directive('highlight',function (el) {
+    let blocks = el.querySelectorAll('pre code');
+
+    let tagsToReplace = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;'
+    };
+
+    function replaceTag(tag) {
+        return tagsToReplace[tag] || tag;
+    }
+
+    function safe_tags_replace(str) {
+        return str.replace(/[&<>]/g, replaceTag);
+    }
+    blocks[0].innerHTML = safe_tags_replace(blocks[0].innerHTML);
+    hljs.highlightBlock(blocks[0]);
+});
 
 const routes = [
     { path: '/head', component: head },
-    { path: '/footer', component: Footer }
+    { path: '/footer', component: Footer },
+    { path: '/nav', component: nav },
+    { path: '/cell', component: cell }
 ];
 const router = new VueRouter({
     routes // （缩写）相当于 routes: routes
@@ -23,5 +48,10 @@ new Vue({
 require('./assets/main.css');
 require('./assets/css/common0.css');
 require('./assets/css/form.css');
+require('./assets/css/bootstrap.min.css');
 require('./assets/css/list.css');
 require('./assets/css/flat-ui.css');
+require('./assets/fonts/iconfont.css');
+require('./assets/jquery-3.0.0.min.js');
+require('./assets/flat-ui.min.js');
+
