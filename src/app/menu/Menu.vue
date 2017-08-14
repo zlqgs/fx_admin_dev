b47e6dc58cd976bcbfc549769bb2e7eeea91b30b
soyo <tuskshenyang@163.com>
2017-08-14 18:13:11 +0800

    菜单引入

--- src/app/menu/Menu.vue
+++ src/app/menu/Menu.vue
@@ -6,8 +6,11 @@
                     <div class="menu" @click="menu_action(index)" :class="{active:(active==index)}">{{ menu.name }}</div>
                 </router-link>
             </div>
-            <div class="menu-second">
-                <div v-for="item in menuList[active].child" class="menu">{{item.name}}</div>
+            <div class="menu-second" data-spy="affix">
+                <template v-for="item in menuList[active].child">
+                    <div class="menu">{{item.name}}</div>
+                    <div class="menu-a" v-for="a in item.child" @click="turn_to(a.href)">{{a.name}}</div>
+                </template>
             </div>
         </div>
         <div class="content">
@@ -68,19 +71,19 @@
                                 child:[
                                     {
                                         name:'单一表头',
-                                        href:null
+                                        href:'#single_head'
                                     },
                                     {
                                         name:'单一表头(带描述)',
-                                        href:null
+                                        href:'#single_head_des'
                                     },
                                     {
                                         name:'多层表头',
-                                        href:null
+                                        href:'#multi_head'
                                     },
                                     {
                                         name:'尾表头',
-                                        href:null
+                                        href:'#end_head'
                                     }
                                 ]
                             },
@@ -89,31 +92,31 @@
                                 child:[
                                     {
                                         name:'文字内容表格',
-                                        href:null
+                                        href:'#word_cell'
                                     },
                                     {
                                         name:'文字内容表格(多行)',
-                                        href:null
+                                        href:'#multi_word_cell'
                                     },
                                     {
                                         name:'分割行',
-                                        href:null
+                                        href:'#clip_line'
                                     },
                                     {
                                         name:'总计',
-                                        href:null
+                                        href:'#total'
                                     },
                                     {
                                         name:'响应文字',
-                                        href:null
+                                        href:'#active_word'
                                     },
                                     {
                                         name:'图片&文字',
-                                        href:null
+                                        href:'#word_pic'
                                     },
                                     {
                                         name:'更多信息',
-                                        href:null
+                                        href:'#more_info'
                                     }
                                 ]
                             },
@@ -122,11 +125,11 @@
                                 child:[
                                     {
                                         name:'横向按钮',
-                                        href:null
+                                        href:"#button_inline"
                                     },
                                     {
                                         name:'纵向按钮',
-                                        href:null
+                                        href:'#button_upright'
                                     }
                                 ]
                             }
@@ -138,19 +141,86 @@
                         child: [
                             {
                                 name:'输入框',
-                                child:[]
+                                child:[
+                                    {
+                                        name:'简单输入框',
+                                        href:"#input"
+                                    },
+                                    {
+                                        name:'长输入框',
+                                        href:"#input_long"
+                                    },
+                                    {
+                                        name:'带单位输入框',
+                                        href:"#input_g"
+                                    },
+                                    {
+                                        name:'多行输入框名称',
+                                        href:"#value_name_double"
+                                    },
+                                    {
+                                        name:'多行文本输入框',
+                                        href:"#textarea"
+                                    },
+                                    {
+                                        name:'标签输入框',
+                                        href:"#tags_input"
+                                    }
+                                ]
                             },
                             {
                                 name:'选择型',
-                                child:[]
+                                child:[
+                                    {
+                                        name:'select',
+                                        href:"#select"
+                                    },
+                                    {
+                                        name:'选择标签1',
+                                        href:"#choice_tag"
+                                    },
+                                    {
+                                        name:'选择标签带扩充',
+                                        href:"#choice_extend"
+                                    },
+                                    {
+                                        name:'Radio',
+                                        href:"#radio"
+                                    },
+                                    {
+                                        name:'CheckBox',
+                                        href:"#check_box"
+                                    },
+                                    {
+                                        name:'性别选择',
+                                        href:"#gender"
+                                    }
+                                ]
                             },
                             {
                                 name:'文件型',
-                                child:[]
-                            },
-                            {
-                                name:'其它',
-                                child:[]
+                                child:[
+                                    {
+                                        name:'头像',
+                                        href:"#head_pic"
+                                    },
+                                    {
+                                        name:'单图',
+                                        href:"#pic_single"
+                                    },
+                                    {
+                                        name:'多图',
+                                        href:"#pic_multi"
+                                    },
+                                    {
+                                        name:'单附件',
+                                        href:"#file_single"
+                                    },
+                                    {
+                                        name:'多附件',
+                                        href:"#file_multi"
+                                    }
+                                ]
                             }
                         ]
                     },
@@ -186,6 +256,9 @@
         methods:{
             menu_action:function (index) {
                 this.active = index;
+            },
+            turn_to:function (href) {
+                $("html,body").animate({scrollTop:$(href).offset().top},300);
             }
         }
     }
@@ -218,6 +291,7 @@
         width: 200px;
         background-color: #FFF;
         box-shadow: 0 0 5px;
+        height: 100%;
     }
     .menu-first .menu{
         color: #AEB7C2;
@@ -241,6 +315,17 @@
         display: flex;
         flex-grow: 1;
         padding: 40px 40px 40px 344px;
+    }
+    .menu-a{
+        display: block;
+        font-size: 1.4rem;
+        padding-left: 40px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        cursor: pointer;
+    }
+    .menu-active{
+        color: #4ab6da;
     }
     .active{
         color: #FFFFFF !important;
