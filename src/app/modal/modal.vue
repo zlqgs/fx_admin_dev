aa5c6afe079a88074e01dcd43abdb2ac7c33da74
soyo <tuskshenyang@163.com>
2017-08-17 10:30:14 +0800

    页面布局样式说明

---
+++ src/app/modal/modal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main-banner">
+        <h6 id="auto_center"><strong>水平活动居中</strong></h6>
+        <blockquote>
+            <h6>样式</h6>
+            <div class="banner-box">
+                <div class="info-table" style="min-height: 0">
+                    <div class="long-form-center">
+                        <div class="appointment-title">
+                            <div class="word">基础信息</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h6>代码</h6>
+            <div v-highlight>
+                <pre>
+                    <code>
+                        <div class="info-table" style="min-height: 0">
+                            <div class="long-form-center">
+                                <div class="appointment-title">
+                                    <div class="word">基础信息</div>
+                                </div>
+                            </div>
+                        </div>
+                    </code>
+                </pre>
+            </div>
+            <h6>说明</h6>
+            <pre>
+                <p>
+                    .info-table 编辑基础页 (form.less)
+                    .long-form-center 编辑页活动居中[max-width:1000px,min-height:600px] (form.less)
+                </p>
+            </pre>
+        </blockquote>
+    </div>
+</template>
